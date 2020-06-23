c9f05589dbf72221a7373b8ea7fcc84c3cda3cb4
Juan Viera <juanviera89@gmail.com>
2020-06-23 17:11:16 -0400

    config

--- db/index.js
+++ db/index.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
-const config = require('config')
+const mongoose = require("mongoose");
+const config = require("config");
 
 const connect = async () => {
-    await mongoose.connect(
-        config.get('db.uri'),
-        config.get('db.options')
-    )
-}
+  await mongoose.connect(
+    "mongodb+srv://pwMaster:R0Xtt2CsgqM3azbqR1kH@pwdevelopment-5gy4a.mongodb.net/payw?retryWrites=true&w=majority",
+    config.get("db.options")
+  );
+};
 
-mongoose.connection.on('disconnected', (event) => {
-    console.error(event)
-    console.error('Database disconection')
-})
+mongoose.connection.on("disconnected", event => {
+  console.error(event);
+  console.error("Database disconection");
+});
 
-module.exports= {
-    connect
-}
+module.exports = {
+  connect
+};
